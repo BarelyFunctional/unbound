b887d07e09a19679e14de9c37eace6b3e396bcfd
Nathan Matthews <nathan.r.matthews@gmail.com>
2012-10-28 09:42:51 +0000

    First major commit of Unbound code

--- src/unbound/core.clj
+++ src/unbound/core.clj
@@ -1,6 +1,91 @@
-(ns unbound.core)
+(ns unbound.core
+  (:use [clojure.set :only (union)]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare unbound-symbols)
+
+(def evens (partial take-nth 2))
+(def odds (comp evens rest))
+
+(defn mapunion [f coll] (apply union (map f coll)))
+
+(defn symbols [form] (filter symbol? (flatten [form])))
+
+(defn unbound-coll-symbols [form] (mapunion unbound-symbols form))
+
+(defmulti seq-syms first)
+
+(defmethod seq-syms 'if [form] (unbound-coll-symbols (rest form)))
+
+(defn rest-fn-syms [form]
+  (letfn [(param-body-syms [[params & body]]
+            (apply disj (unbound-coll-symbols body) (symbols params)))
+          (syms [form]
+            (if (vector? (first form))
+              (param-body-syms form)
+              (mapunion param-body-syms form)))]
+    (if (symbol? (first form))
+      (disj (syms (rest form)) (first form))
+      (syms form))))
+
+(defmethod seq-syms 'fn* [form]
+  (rest-fn-syms (rest form)))
+
+(defmethod seq-syms 'letfn* [[_ fns & body]]
+  (apply disj
+         (union
+          (mapunion unbound-symbols (odds fns))
+          (unbound-coll-symbols body))
+         (evens fns)))
+
+(defn rest-binding-syms [[bindings & body]]
+  (apply union
+   (apply disj
+          (unbound-coll-symbols body)
+          (evens bindings))
+   (map
+    (partial apply disj)
+    (map unbound-symbols (odds bindings))
+    (reductions conj [] (evens bindings)))))
+
+(defmethod seq-syms 'quote [form] #{})
+
+(defmethod seq-syms 'let* [form]
+  (rest-binding-syms (rest form)))
+
+(defmethod seq-syms 'new [form] (unbound-coll-symbols (drop 2 form)))
+(defmethod seq-syms 'recur [form] (unbound-coll-symbols (rest form)))
+(defmethod seq-syms 'do [form] (unbound-coll-symbols (rest form)))
+
+(defmethod seq-syms 'try [form]
+  (let [catch? (every-pred seq? not-empty #(or (= 'catch (first %))))
+        finally? (every-pred seq? not-empty #(= 'finally (first %)))]
+    (union
+     (unbound-coll-symbols (take-while (comp not (some-fn catch? finally?)) (rest form)))
+     (mapunion
+      (fn [[_ _ e & body]] (disj (unbound-coll-symbols body) e)) (filter catch? form))
+     (mapunion
+      (comp unbound-coll-symbols rest) (filter finally? form))
+     )))
+
+(defmethod seq-syms 'def [form] (disj (unbound-symbols (last form)) (second form)))
+
+(defmethod seq-syms 'loop* [form]
+  (rest-binding-syms (rest form)))
+
+(defmethod seq-syms '. [form]
+  (union
+   (unbound-symbols (second form))
+   (unbound-coll-symbols
+    (if (symbol? (nth form 2))
+      (drop 3 form)
+      (rest (nth form 2))))))
+
+(defmethod seq-syms :default [form] (unbound-coll-symbols form))
+
+(defn unbound-symbols [form]
+  (let [eform (macroexpand form)]
+    (condp #(%1 %2) eform
+      seq?  (if (empty? eform) #{} (seq-syms eform))
+      coll?  (apply union (map unbound-symbols eform))
+      symbol? #{eform}
+      #{})))
--- test/unbound/core_test.clj
+++ test/unbound/core_test.clj
@@ -2,6 +2,17 @@
   (:use clojure.test
         unbound.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test1
+  (testing "collections excluding seqs"
+    (is (= (unbound-symbols ['a 'b 1 2 'c]) #{'a 'b 'c}))
+    (is (= (unbound-symbols [['a 'b 1] 'c 'a 'd]) #{'a 'b 'c 'd}))
+    (is (= (unbound-symbols #{'a 'b 'c}) #{'a 'b 'c}))
+    (is (= (unbound-symbols {:a 'a :b 'b 'c 'd}) #{'a 'b 'c 'd})))
+  (testing "simple functions"
+    (is (= (unbound-symbols '(fn [x] (* x y 2))) #{'* 'y})))
+  (testing "letfn forms"
+    (is (= (unbound-symbols '(letfn [(x [y] (* y 2))] (* x y a))) #{'y 'a '*})))
+  (testing "let forms"
+    (is (= (unbound-symbols '(let [a (+ b d g) d 3] (+ a b c d e))) #{'+ 'c 'b 'g 'd 'e})))
+  (testing "try catch"
+    (is (= (unbound-symbols '(try (+ 1 2 a) (catch Throwable e z)))))))
